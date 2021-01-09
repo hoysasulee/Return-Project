30e7f8eed2520671d0f6efce0f53db00a086dfa5
hoysasulee <hoy1922@yahoo.com>
2021-01-09 20:33:44 +1300

    Create Mathematic statistic practice_1.r

---
+++ Mathematic statistic practice_1.r
@@ -0,0 +1,6 @@
+# Consider the function defined by:
+\begin{equation}
+\ f(x) = \sin(\sin(x^2)) + \cos(\frac{1}{1+x+x^2})-1
+\end{equation}
+
+Translate this into R idiom and plot it for x ∈ [−2, 2].
